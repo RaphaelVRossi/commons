c064d3ede24ed22fcb69e6a688fd9716a2160d95
raphael.rossi <raphael.rossi@engdb.com.br>
2021-09-26 17:41:56 -0300

    Initial Commit

---
+++ src/test/java/br/com/rvr/services/commons/CommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rvr.services.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
